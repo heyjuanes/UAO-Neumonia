801e7b788018ae6d5f1ec8607c9990fda719f88e
Juan Esteban Espitia <juanchoespitia1@gmail.com>
2026-02-11 05:09:28 -0500

    Load model without compile to fix legacy reduction error

--- src/load_model.py
+++ src/load_model.py
@@ -1,23 +1,20 @@
 """
 Módulo para cargar el modelo de neumonía entrenado (.h5).
 """
-#----
-import tensorflow as tf
-from keras import backend as K
-from keras import load_model
-#----
+
+from tensorflow.keras.models import load_model # type: ignore
+
 _MODEL = None
+
 
 def load_cnn_model(model_path: str):
     """
     Carga el modelo CNN desde un archivo .h5.
     El modelo se carga una sola vez (singleton).
-    
-    :param model_path: Ruta al archivo .h5
-    :return: Modelo Keras cargado
     """
     global _MODEL
 
     if _MODEL is None:
-        _MODEL = load_model(model_path)
+        _MODEL = load_model(model_path, compile=False)
+
     return _MODEL
