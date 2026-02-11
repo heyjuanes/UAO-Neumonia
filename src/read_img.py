5c61602bf2364eb4beff0f514256041fd412e04c
Juan Esteban Espitia <juanchoespitia1@gmail.com>
2026-02-11 05:07:19 -0500

    Fix return values in read_img module

--- src/read_img.py
+++ src/read_img.py
@@ -1,24 +1,38 @@
 import cv2
 import numpy as np
+from PIL import Image
+import pydicom
 
 
-def preprocess_img(array):
+def read_dicom_file(path: str):
     """
-    Preprocesa la imagen:
-    - Resize a 512x512
-    - Escala de grises
-    - CLAHE
-    - Normalización [0,1]
-    - Conversión a batch (tensor)
+    Lee imagen DICOM y la convierte a formato RGB numpy.
     """
-    array = cv2.resize(array, (512, 512))
-    array = cv2.cvtColor(array, cv2.COLOR_BGR2GRAY)
+    dicom_data = pydicom.dcmread(path)
+    img_array = dicom_data.pixel_array
 
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(4, 4))
-    array = clahe.apply(array)
+    img2show = Image.fromarray(img_array)
 
-    array = array / 255.0
-    array = np.expand_dims(array, axis=-1)
-    array = np.expand_dims(array, axis=0)
+    img_array = img_array.astype(float)
+    img_array = (np.maximum(img_array, 0) / img_array.max()) * 255.0
+    img_array = np.uint8(img_array)
 
-    return array
+    img_rgb = cv2.cvtColor(img_array, cv2.COLOR_GRAY2RGB)
+
+    return img_rgb, img2show
+
+
+def read_jpg_file(path: str):
+    """
+    Lee imagen JPG/PNG y la convierte a numpy array.
+    """
+    img = cv2.imread(path)
+    img_array = np.asarray(img)
+
+    img2show = Image.fromarray(img_array)
+
+    img_array = img_array.astype(float)
+    img_array = (np.maximum(img_array, 0) / img_array.max()) * 255.0
+    img_array = np.uint8(img_array)
+
+    return img_array, img2show
